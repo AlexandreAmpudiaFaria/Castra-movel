d1d79608456e0f1188eb86359234afc7a19baba5
Alexandre <alexandre_faria123@icloud.com>
2021-02-08 21:08:17 -0300

    update

---
+++ Castra-Movel-Civap/Castra-Movel-Civap/src/main/java/com/example/CastraMovelCivap/CastraMovelCivapApplication.java
@@ -0,0 +1,13 @@
+package com.example.CastraMovelCivap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastraMovelCivapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastraMovelCivapApplication.class, args);
+	}
+
+}
---
+++ Castra-Movel-Civap/Castra-Movel-Civap/src/test/java/com/example/CastraMovelCivap/CastraMovelCivapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CastraMovelCivap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastraMovelCivapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
